c638be42e01f9097db2cb3ac1ee1a48ba8e51873
jo0wi <jrw22710@uga.edu>
2025-12-30 11:23:06 -0500

    add testbench for UART_RX module

---
+++ HDL_Projects/uart transceiver/uart transceiver.srcs/sim_1/new/tb_UART_RX.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/30/2025 11:19:20 AM
+// Design Name: 
+// Module Name: tb_UART_RX
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_UART_RX(
+
+    );
+endmodule
